55e1c857b267c4863823081a1649d1e4e4208574
Zuzana <zuzana.roh@gmail.com>
2026-05-11 14:31:24 +0200

    Calcualtion of overlapping, without width math

--- main.js
+++ main.js
@@ -22,25 +22,41 @@ for (let i = 0; i < eventsData.length; i++) {
     let j = 0;
 
     let startSlot = 0;
+    let overlap = 0;
 
     while (j < slotEndDates.length) {
-        if (event.starttime >= slotEndDates[j]) {
+        if (event.starttime >= slotEndDates[j] && overlap === 0) {
             slotEndDates[j] = event.end;
-            break;
+            console.log("overlap")
+            startSlot = j;
+            overlap++;
+            j++;
+            continue;
+            // break;
+        }
+
+        if (overlap > 0 && event.starttime < slotEndDates[j]) {
+            overlap++;            
         }
         j++;
     }
 
     if (j >= slotEndDates.length) {
+
+        if (overlap === 0) {
+            overlap++;
+            startSlot = j;
+        }
+        
         slotEndDates.push(event.end);
     }
 
-    console.log(event.value, j);
+    console.log(event.value, startSlot, overlap);
     console.log(event.starttime.toLocaleTimeString())
     console.log(event.end.toLocaleTimeString())
     console.log("-----")
 
-    events.push({ ...event, j });
+    events.push({ ...event, startSlot, overlap });
 }
 
 for (const event of events) {
@@ -58,15 +74,14 @@ for (const event of events) {
     const duration = Math.round((event.end.getTime() - event.starttime.getTime()) / 60000);
 
     eventDiv.innerText = event.value;
-    //eventDiv.style.width = `${100 / g.length}%`;
-    eventDiv.style.width = "50px";
-    eventDiv.style.height = 60 * duration / 60 + "px"; // duration in minutes + borders
+    eventDiv.style.width = `${100 / slotEndDates.length * event.overlap}%`;
+    eventDiv.style.height = 60 * duration / 60 + "px"; // duration in minutes
     eventDiv.style.backgroundColor = "hsl(" + Math.random() * 360 + ", 100%, 75%)";
 
 
     eventDiv.style.position = "absolute";
     eventDiv.style.top = 20 + (startHour - 7) + (startHour - 7) * 60 + (startMinute * 60 / 60) + "px"; // header size + something with borders? + moved hour AND PLUS minutes offset
-    eventDiv.style.left = 60 * event.j + "px";
+    eventDiv.style.left = 60 * event.startSlot + "px";
     dayDiv.appendChild(eventDiv);
 
 }
