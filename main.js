29f96761ebe7ff2525c79f79e9386506b35eb6ec
Zuzana <zuzana.roh@gmail.com>
2026-05-12 14:59:36 +0200

    Switching between weeks

--- main.js
+++ main.js
@@ -1,12 +1,79 @@
+// CALENDAR 
+const currentDate = new Date();
+const first = currentDate.getDate() - currentDate.getDay() + 1;
+
+const monday = new Date(currentDate.setDate(first))
+const sunday = new Date(currentDate.setDate(first + 6))
+
+const dateHeaders = document.getElementsByClassName("calendar-header-week-date")[0];
+dateHeaders.innerText = `${monday.toLocaleDateString()} - ${sunday.toLocaleDateString()}`;
+
+
+const prevButton = document.getElementById("previousButton");
+prevButton.addEventListener('click', (e) => {
+    const currentWeek = new Date(document.getElementsByClassName("calendar-day-column")[0].id);
+    currentWeek.setDate(currentWeek.getDate() - 7);
+    renderCalendar(currentWeek);
+
+});
+
+const nextButton = document.getElementById("nextButton");
+nextButton.addEventListener('click', (e) => {
+    const currentWeek = new Date(document.getElementsByClassName("calendar-day-column")[0].id);
+    currentWeek.setDate(currentWeek.getDate() + 7);
+    renderCalendar(currentWeek);
+
+});
+
+
+const daysHeader = document.getElementsByClassName("calendar-days-header")[0];
+
+const options = { weekday: "short" };
+
+for (let i = 0; i < 7; i++) {
+    const day = new Date(currentDate.setDate(first + i))
+    const weekColumn = document.createElement("div");
+    weekColumn.classList.add("calendar-day-column");
+    weekColumn.id = `${new Date(day).toLocaleDateString()}`;
+    weekColumn.innerText = `${new Intl.DateTimeFormat("sv-SE", options).format(day)}, ${day.getDate()}`;
+    daysHeader.appendChild(weekColumn);
+}
+
+function renderCalendar(firstDayDate) {
+    // changing week
+    let lastDayDate = new Date(firstDayDate)
+    lastDayDate.setDate(lastDayDate.getDate() + 6)
+
+    const dateHeaders = document.getElementsByClassName("calendar-header-week-date")[0];
+    dateHeaders.innerText = `${firstDayDate.toLocaleDateString()} - ${lastDayDate.toLocaleDateString()}`;
+
+
+    // changing columns
+    const daysHeader = document.getElementsByClassName("calendar-day-column");
+
+    for (let i = 0; i < 7; i++) {
+        const day = new Date(firstDayDate);
+         day.setDate(day.getDate() + i);
+
+        daysHeader[i].id = `${day.toLocaleDateString()}`;
+        daysHeader[i].innerText = `${new Intl.DateTimeFormat("sv-SE", options).format(day)}, ${day.getDate()}`;
+    }
+
+    
+}
+
+
+
+// EVENTS
 const eventsData = [
     // { value: "Activity", starttime: "10:00", end: "11:00", duration: 60, day: "Mon" },
     // { value: "Lab", starttime: "12:30", end: "14:00", duration: 90, day: "Tue" },
     // { value: "Lab", starttime: "12:50", end: "13:30", duration: 40, day: "Tue" },
-    { value: "A", starttime: new Date("2026-05-11T09:00:00"), end: new Date("2026-05-11T10:00:00"), duration: 60, day: "Thu" },
-    { value: "B", starttime: new Date("2026-05-11T09:30:00"), end: new Date("2026-05-11T10:30:00"), duration: 60, day: "Thu" },
-    { value: "C", starttime: new Date("2026-05-11T09:45:00"), end: new Date("2026-05-11T12:00:00"), duration: 120, day: "Thu" },
-    { value: "D", starttime: new Date("2026-05-11T11:00:00"), end: new Date("2026-05-11T11:30:00"), duration: 30, day: "Thu" },
-    { value: "E", starttime: new Date("2026-05-11T11:30:00"), end: new Date("2026-05-11T12:00:00"), duration: 30, day: "Thu" },
+    { value: "A", starttime: new Date(2026, 4, 11, 9, 0), end: new Date(2026, 4, 11, 10, 0), duration: 60, day: "Thu" },
+    { value: "B", starttime: new Date(2026, 4, 11, 9, 30), end: new Date(2026, 4, 11, 10, 30), duration: 60, day: "Thu" },
+    { value: "C", starttime: new Date(2026, 4, 11, 9, 45), end: new Date(2026, 4, 11, 12, 0), duration: 120, day: "Thu" },
+    { value: "D", starttime: new Date(2026, 4, 11, 11, 0), end: new Date(2026, 4, 11, 11, 30), duration: 30, day: "Thu" },
+    { value: "E", starttime: new Date(2026, 4, 11, 11, 30), end: new Date(2026, 4, 11, 12, 0), duration: 30, day: "Thu" },
 ]
 
 
@@ -27,7 +94,6 @@ for (let i = 0; i < eventsData.length; i++) {
     while (j < slotEndDates.length) {
         if (event.starttime >= slotEndDates[j] && overlap === 0) {
             slotEndDates[j] = event.end;
-            console.log("overlap")
             startSlot = j;
             overlap++;
             j++;
@@ -36,7 +102,7 @@ for (let i = 0; i < eventsData.length; i++) {
         }
 
         if (overlap > 0 && event.starttime < slotEndDates[j]) {
-            overlap++;            
+            overlap++;
         }
         j++;
     }
@@ -47,24 +113,21 @@ for (let i = 0; i < eventsData.length; i++) {
             overlap++;
             startSlot = j;
         }
-        
+
         slotEndDates.push(event.end);
     }
 
-    console.log(event.value, startSlot, overlap);
-    console.log(event.starttime.toLocaleTimeString())
-    console.log(event.end.toLocaleTimeString())
-    console.log("-----")
+    // console.log(event.value, startSlot, overlap);
+    // console.log(event.starttime.toLocaleTimeString())
+    // console.log(event.end.toLocaleTimeString())
+    // console.log("-----")
 
     events.push({ ...event, startSlot, overlap });
 }
 
 for (const event of events) {
-    console.log(event)
-}
-
-for (const event of events) {
-    const dayDiv = document.getElementById(event.day); // this shouold be replaced by proper date later, but for now..
+    const eventDate = event.starttime.toLocaleDateString();
+    const dayDiv = document.getElementById(eventDate);
 
     const eventDiv = document.createElement("div");
 
